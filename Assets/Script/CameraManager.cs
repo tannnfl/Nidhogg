35070e03294059aba78bed51be2d9045af955235
harper <woshishencaiyi@163.com>
2024-04-06 22:41:50 -0400

    Updated player leaving camera border check

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -10,12 +10,16 @@ public class CameraManager : MonoBehaviour
     public GameObject RightPlayer;
     public GameObject PlayerMidPoint;
 
+    //Update the followtarget for the virtual cam
     public Transform followTarget;
 
-    //to take whatever state is current in GameManager
+    //take whatever state that is current in the GameManager
     public GOState currentGOState;
 
+    //get virtual cam and cam width
     private CinemachineVirtualCamera playerVirtualCam;
+    private Camera cam;
+    private float camHalfWidth;
 
     //remember that Leftplayer always GORight, and Rightplayer always GOLeft!!!
     public enum GOState
@@ -28,6 +32,9 @@ public class CameraManager : MonoBehaviour
     public void Start()
     {
         playerVirtualCam = GetComponent<CinemachineVirtualCamera>();
+
+        cam = Camera.main;
+        camHalfWidth = cam.aspect * cam.orthographicSize;
     }
 
     public void Update()
@@ -45,6 +52,14 @@ public class CameraManager : MonoBehaviour
                 if (LeftPlayer != null)
                 {
                     followTarget = PlayerMidPoint.transform;
+                    var distance = LeftPlayer.transform.position.x - PlayerMidPoint.transform.position.x;
+                    //if distance indicates the leftplayer is heading left, it cannot move once reaching the point where rightplayer would be left out
+                    //if distance indicates the leftplayer is heading right, the camera shifts and a new right player would spawn on the right
+                    if (distance > camHalfWidth)
+                    {
+                        //out of camera border
+                        //actually this part has little camera behavior, it's just respawning the rightplayer on the right side of the leftplayer
+                    }
                 }
                     break;
 
